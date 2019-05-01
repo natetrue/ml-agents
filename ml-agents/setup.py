5df9e5b1aeea34cff489a5c421ae5cc9e35552f7
Nate True <nate.true@synapse.com>
2019-05-01 06:15:49 -0700

    Upgrade to latest Unity, Python, and Tensorflow

--- ml-agents/setup.py
+++ ml-agents/setup.py
@@ -30,19 +30,19 @@ setup(
 
     install_requires=[
         'mlagents_envs==0.8.1',
-        'tensorflow>=1.7,<1.8',
+        'tensorflow>=1.7',
         'Pillow>=4.2.1',
         'matplotlib',
-        'numpy>=1.13.3,<=1.14.5',
+        'numpy>=1.13.3',
         'jupyter',
         'pytest>=3.2.2,<4.0.0',
         'docopt',
         'pyyaml',
-        'protobuf>=3.6,<3.7',
-        'grpcio>=1.11.0,<1.12.0',
+        'protobuf>=3.6',
+        'grpcio>=1.11.0',
         'pypiwin32==223;platform_system=="Windows"'],
 
-    python_requires=">=3.6,<3.7",
+    python_requires=">=3.6",
 
     entry_points={
         'console_scripts': [
